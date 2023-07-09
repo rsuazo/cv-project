68314a793c6585d8532783050fa2932358096835
rsuazo <rsuazo2@gmail.com>
2023-07-09 15:52:02 -0500

    Convert class component to functional component

--- src/components/generalinfo.js
+++ src/components/generalinfo.js
@@ -1,66 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class GeneralInfo extends Component {
-  constructor(props) {
-    super(props);
+const GeneralInfo = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [edit, setEdit] = useState(false);
 
-    this.state = {
-      name: '',
-      email: '',
-      phone: '',
-      edit: false
-    }
+  const updateName = (e) => {
+    setName(e.target.value);
+  }
+  
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
   }
 
-  handleChange = (e) => {
-    this.setState({
-        [e.target.name]: e.target.value,
-    })
+  const updatePhone = (e) => {
+    setPhone(e.target.value);
   }
 
-  toggleEdit = (e) => {
-    this.setState(
-      prevState => ({
-        edit: !prevState.edit
-      })
-    )
+  const toggleEdit = (e) => {
+    setEdit(!edit);
   }
 
-  render() {
-    const { name, email, phone, edit } = this.state;
-    return (
-      <div>
-        <h2>Personal Information</h2>
-        <h3>{name ? name : 'Your name'}</h3>
-        <h3>{email ? email : 'Email'}</h3>
-        <h3>{phone ? phone : 'Phone'}</h3>
-       {!edit &&
+  return (
+    <div>
+      <h2>Personal Information</h2>
+      <h3>{name ? name : 'Your name'}</h3>
+      <h3>{email ? email : 'Email'}</h3>
+      <h3>{phone ? phone : 'Phone'}</h3>
+      {!edit &&
+        <button
+            className="edit-toggle"
+            type="button"
+            onClick={toggleEdit}
+        >
+          Edit
+        </button>
+      }
+
+      {edit && 
+        <form id="form_generalInfo">
+          <input type="text" id="name" name="name" onChange={updateName} placeholder="Name" ></input><br></br>
+          <input type="email" id="email" name="email" onChange={updateEmail} placeholder="Email"></input><br></br>
+          <input type="tel" id="phone" name="phone"onChange={updatePhone} placeholder="Phone"></input><br></br><br></br>
           <button
-              className="edit-toggle"
               type="button"
-              onClick={this.toggleEdit}
+              onClick={toggleEdit}
+              className='close'
           >
-            Edit
+            Close Edit
           </button>
-        }
-
-        {edit && 
-          <form id="form_generalInfo">
-            <input type="text" id="name" name="name" onChange={this.handleChange} placeholder="Name" ></input><br></br>
-            <input type="email" id="email" name="email" onChange={this.handleChange} placeholder="Email"></input><br></br>
-            <input type="tel" id="phone" name="phone"onChange={this.handleChange} placeholder="Phone"></input><br></br><br></br>
-            <button
-                type="button"
-                onClick={this.toggleEdit}
-                className='close'
-            >
-              Close Edit
-            </button>
-          </form>
-        }
-      </div>
-    );
-  }
+        </form>
+      }
+    </div>
+  );
 }
 
 export default GeneralInfo;
