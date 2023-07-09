d25c182755a0293e23447d130ebc1f5cbafa38f8
rsuazo <rsuazo2@gmail.com>
2023-07-09 16:47:56 -0500

    convert practicalexperience to funct component

--- src/components/practicalexperience.js
+++ src/components/practicalexperience.js
@@ -1,73 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class PracticalExperience extends Component {
-  constructor(props) {
-    super(props);
+const PracticalExperience = () => {
 
-    this.state = {
-      companyName: '',
-      positionTitle: '',
-      responsibilities: '',
-      startDate: '',
-      endDate: '',
-      edit: false
-    }
+  const [companyName, setCompanyName] = useState('');
+  const [positionTitle, setPositionTitle] = useState('');
+  const [responsibilities, setResponsibilities] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [edit, setEdit] = useState(false);
+
+  const updateCompanyName = (e) => {
+    setCompanyName(e.target.value);
+  }
+  
+  const updatePositionTitle = (e) => {
+    setPositionTitle(e.target.value);
   }
 
-  handleChange = (e) => {
-    this.setState({
-        [e.target.name]: e.target.value,
-    })
+  const updateResponsibilities = (e) => {
+    setResponsibilities(e.target.value);
   }
 
-  toggleEdit = (e) => {
-    this.setState(
-      prevState => ({
-        edit: !prevState.edit
-      })
-    )
+  const updateStartDate = (e) => {
+    setStartDate(e.target.value);
   }
 
-  render() {
-    const { companyName, positionTitle, responsibilities, startDate, endDate, edit } = this.state;
-    return (
-      <div>
-        <h2>Personal Information</h2>
-        <h3>{companyName ? companyName : 'Company Name'}</h3>
-        <h3>{positionTitle ? positionTitle : 'Positition Title'}</h3>
-        <h3>{responsibilities ? responsibilities : 'Responsibilities'}</h3>
-        <h3>{startDate ? startDate : 'Start Date'}</h3>
-        <h3>{endDate ? endDate : 'End Date'}</h3>
-        {!edit &&
+  const updateEndDate = (e) => {
+    setEndDate(e.target.value);
+  }
+  
+  const toggleEdit = (e) => {
+    setEdit(!edit);
+  }
+
+  return (
+    <div>
+      <h2>Personal Information</h2>
+      <h3>{companyName ? companyName : 'Company Name'}</h3>
+      <h3>{positionTitle ? positionTitle : 'Positition Title'}</h3>
+      <h3>{responsibilities ? responsibilities : 'Responsibilities'}</h3>
+      <h3>{startDate ? startDate : 'Start Date'}</h3>
+      <h3>{endDate ? endDate : 'End Date'}</h3>
+      {!edit &&
+        <button
+            className="edit-toggle"
+            type="button"
+            onClick={toggleEdit}
+        >
+          Edit
+        </button>
+      }
+
+      {edit && 
+        <form name="form_practicalExperience">
+          <input type="text" id="company_name" name="companyName" placeholder='Company Name' onChange={updateCompanyName}></input><br></br>
+          <input type="text" id="position_title" name="positionTitle" placeholder='Position Title' onChange={updatePositionTitle}></input><br></br>
+          <input type="text" id="responsibilities" name="responsibilities" placeholder='Responsibilities' onChange={updateResponsibilities}></input><br></br>
+          <input type="date" id="start_date" name="startDate" placeholder='Start Date' onChange={updateStartDate}></input><br></br>
+          <input type="date" id="end_date" name="endDate" placeholder='End Date' onChange={updateEndDate}></input><br></br>
           <button
-              className="edit-toggle"
               type="button"
-              onClick={this.toggleEdit}
+              onClick={toggleEdit}
+              className='close'
           >
-            Edit
+            Close Edit
           </button>
-        }
-
-        {edit && 
-          <form name="form_practicalExperience">
-            <input type="text" id="company_name" name="companyName" placeholder='Company Name' onChange={this.handleChange}></input><br></br>
-            <input type="text" id="position_title" name="positionTitle" placeholder='Position Title' onChange={this.handleChange}></input><br></br>
-            <input type="text" id="responsibilities" name="responsibilities" placeholder='Responsibilities' onChange={this.handleChange}></input><br></br>
-            <input type="date" id="start_date" name="startDate" placeholder='Start Date' onChange={this.handleChange}></input><br></br>
-            <input type="date" id="end_date" name="endDate" placeholder='End Date' onChange={this.handleChange}></input><br></br>
-            <button
-                type="button"
-                onClick={this.toggleEdit}
-                className='close'
-            >
-              Close Edit
-            </button>
-          </form>
-          
-        }
-      </div>
-    );
-  }
+        </form>
+        
+      }
+    </div>
+  );
 }
 
 export default PracticalExperience;
